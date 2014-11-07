f6a23c5fc7af03bdfc1be13809b6ab37638be879
BestKind <xingang_tian@163.com>
2014-11-07 19:34:54 +0800

    send email and some option of mysql
    send email and some option of mysql

---
+++ sendEmail/config.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+//使用163邮箱服务器
+const SMTPSERVER = "smtp.163.com";
+//163邮箱服务器端口
+const SMTPSERVERPORT = 25;
+//你的163服务器邮箱账号
+const SMTPUSERMAIL = "****@163.com";
+//你的邮箱账号(去掉@163.com)
+const SMTPUSER = "****";
+//你的邮箱密码
+const SMTPPASS = "****";
+//邮件主题 
+const MAILSUBJECT = "发一封测试一下";
+
+//这里面的一个true是表示使用身份验证,否则不使用身份验证. 
+$smtp = new smtp(SMTPSERVER, SMTPSERVERPORT, true, SMTPUSER, SMTPPASS);
+//是否显示发送的调试信息 
+$smtp->debug = false;
+
+?>
---
+++ sendEmail/index.php
@@ -0,0 +1,16 @@
+﻿<?php
+require_once 'smtp.php';
+require_once 'config.php';
+
+
+//收件人邮箱
+$smtpemailto = "***@***";
+
+//邮件内容 
+$mailbody = "HELLO, 测试成功了哟!";
+//邮件格式（HTML/TXT）,TXT为文本邮件 
+$mailtype = "TXT";
+
+//发送邮件
+$smtp->sendmail($smtpemailto, SMTPUSERMAIL, MAILSUBJECT, $mailbody, $mailtype);
+?>
